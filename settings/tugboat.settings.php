2d895e3af0aa3ecd1ce5c952a671a46b4039c4ae
Dane Powell <git@danepowell.com>
2019-06-28 16:06:31 -0700

    Added detector plugin.

---
+++ settings/tugboat.settings.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Tugboat settings.
+ */
+
+$databases = [
+  'default' => [
+    'default' => [
+      'database' => 'asdf',
+      'username' => 'tugboat',
+      'password' => 'tugboat',
+      'host' => 'mysql',
+      'port' => '3306',
+      'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ],
+  ],
+];
